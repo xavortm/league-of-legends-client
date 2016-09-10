4820a6ac4b5c7418275645f34eadebac185e9e10
xavortm <xavortm@gmail.com>
2016-09-10 11:23:07 +0300

    Add gulp and minor updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,14 @@ $(window).load(function () {
 jQuery( document ).ready( function ( $ ) {
 	'use strict';
 
-	
+	// In miliseconds how long to wait before showing the screen.
+	var LOAD_DELAY_TIME = 1000;
+
+	// This batch of constants will fire after LOAD_DELAY_TIME, so
+	// 0ms == 1000ms from .ready()
+	var LOAD_DELAY_TIME_CHAMPIONS_PANEL_BLUE = 500;
+	var LOAD_DELAY_TIME_CHAMPIONS_PANEL_RED = 500;
+	var LOAD_DELAY_TIME_CHAMPIONS_GRID = 1000;
 
 	/*
 	|--------------------------------------------------------------------------
@@ -56,7 +63,8 @@ jQuery( document ).ready( function ( $ ) {
 		};
 
 		/**
-		 * Fire all functions that will be used in the page.
+		 * Fire all functions that will be used in the page. Having them all here
+		 * will ease up finding what fires certain events.
 		 */
 		var events = function () {
 			$('.champions-grid .champ-icon:not(.inactive)').click(selectChampion);
@@ -66,21 +74,56 @@ jQuery( document ).ready( function ( $ ) {
 			$('.chat-submit').click(chatSubmit);
 		};
 
-
+		/**
+		 * Start building visually the interface. The thing with this is
+		 * that the viewer has to see how the client view comes to life.
+		 */
 		var init = function () {
-			loadIcons('.champions-grid .champ-icon');
-			loadIcons('.team-blue .champion-panel');
-			loadIcons('.team-red .champion-panel');
+			// load_delay_time
+			setTimeout(function() {
+				loadIcons('.champions-grid .champ-icon', 1000);
+			}, LOAD_DELAY_TIME_CHAMPIONS_GRID);
+
+			setTimeout(function() {
+				loadIcons('.team-blue .champion-panel', 2000);
+			}, LOAD_DELAY_TIME_CHAMPIONS_PANEL_BLUE);
+
+			setTimeout(function() {
+				loadIcons('.team-red .champion-panel', 2000);
+			}, LOAD_DELAY_TIME_CHAMPIONS_PANEL_RED);
+
+			var messages = {
+				"notaReaper": "I like turtles :)",
+				"Mangak8": "y did u ban corki?",
+				"Mangak8": "I will play mid",
+				"FullMetalBr0": "hecarim mid ftw, gona rekt em",
+				"notaReaper": "I like trains :)",
+				"FullMetalBr0": "feed or troll 24/7 bruh",
+				"Extenz": "We are gonna loose this one..",
+				"notaReaper": "I like to smell glue :)",
+				"FullMetalBr0": "reaper shut up retard",
+				"Extenz": "I always get to play with idiots",
+				"FullMetalBr0": "extenz just for you i will feed",
+				"Extenz": "This is not happening right now ...",
+				"Extenz": "Hecarim reported"
+			};
+
+			// console.log(messages);
 
 			loadTimer();
+			// for( var msg in messages ) writeComments(messages, msg, 0);
 			commentsForm();
 		}
 
+		/**
+		 * Clicking on the available champions must select them
+		 * for the user playing the game.
+		 * 
+		 * @param  {obj} event The event (click in this case)
+		 */
 		var selectChampion = function (event) {
 			var clickedImgUrl = $(event.toElement).attr('src');
 			var currentImgUrl = $('.current-player .champ-icon img').attr('src');
-
-			console.log(currentImgUrl);
 
 			if( $('.current-player .champ-icon img').length == 0 || currentImgUrl != clickedImgUrl ) {
 				$('.current-player .champ-icon img').remove();
@@ -88,13 +131,35 @@ jQuery( document ).ready( function ( $ ) {
 			}
 		}
 
+		/**
+		 * Write some comments in the comment field to show interactivity.
+		 */
+		var writeComments = function( messages, msg, item ) {
+			console.log(messages);
+		}
+
+		/**
+		 * Toggle between "selected" class when picking a champion.
+		 * This will allow to style it from CSS.
+		 */
 		var toggleTags = function () {
 			$(this).siblings().removeClass('selected');
 			$(this).addClass('selected');
 		}
 
-		var loadIcons = function ( element ) {
+		/**
+		 * Iterate through the champions and add .loaded class each 100 seconds.
+		 * This will create smooth animation of showing them one by one.
+		 * 
+		 * @param  {string} The class for the icons.
+		 */
+		var loadIcons = function ( element, totalLoadTime ) {
 			var time = 0;
+			var delay = 100;
+
+			if (undefined !== totalLoadTime) {
+				delay = totalLoadTime / $(element).length;
+			}
 
 			$(element).each(function( el ) {
 				var currElement = $(this);
@@ -103,7 +168,7 @@ jQuery( document ).ready( function ( $ ) {
 					currElement.addClass('loaded');
 				}, time);
 
-				time += 100;
+				time += delay;
 			});
 		}
 
@@ -115,8 +180,7 @@ jQuery( document ).ready( function ( $ ) {
 				}
 				$('.seconds').text(_s.currentSeconds--);
 
-
-			}, 1000);
+			}, LOAD_DELAY_TIME);
 
 		}
 
@@ -147,9 +211,14 @@ jQuery( document ).ready( function ( $ ) {
 					$('.chat-messages').append('<span class="chat-msg"><strong class="user">XavorTM:</strong> ' + inputContent + '</span>');
 					$('.chatbox-input').val('');
 				}
+
+				$(".chat-messages").scrollTop($(".chat-messages")[0].scrollHeight);;
 			});
 		}
 
+		/**
+		 * Add message in the chat by appending <span> on enter.
+		 */
 		var chatSubmit = function() {
 			var inputContent = $('.chatbox-input').val();
 
@@ -171,12 +240,15 @@ jQuery( document ).ready( function ( $ ) {
 
 	})();
 
+	/**
+	 * How long to wait before adding the visible class
+	 */
 	setTimeout(function() {
 		siteScripts.watchEvents(); // Begin watching for events.
 		siteScripts.init();
 
 		$('.client-window').addClass('visible');
-	}, 1000);
+	}, LOAD_DELAY_TIME);
 	
 
 });
